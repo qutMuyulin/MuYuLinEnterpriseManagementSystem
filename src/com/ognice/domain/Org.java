83e0d31c5b796f580b1c10b771f245e4d4d9da9b
Mu YuLin <myl123567@126.com>
2020-05-17 09:31:11 +0800

    工种实体类

---
+++ src/com/ognice/domain/Org.java
@@ -0,0 +1,30 @@
+package com.ognice.domain;
+
+import java.util.Date;
+/**
+*
+* 部门 实体类
+*
+**/
+public class Org {
+    private Integer id;
+
+	public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    private String name;
+
+	public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
